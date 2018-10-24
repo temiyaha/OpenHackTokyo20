da0a13b16f68d167e95634b38c9a70efbeb9ec25
ykomori1023 <44392646+ykomori1023@users.noreply.github.com>
2018-10-24 16:18:01 +0900

    CreateRating

--- Ratings/Ratings/CreateRating.cs
+++ Ratings/Ratings/CreateRating.cs
@@ -1,43 +1,3 @@
-//using System;
-//using System.IO;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.Azure.WebJobs;
-//using Microsoft.Azure.WebJobs.Extensions.Http;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.Extensions.Logging;
-//using Newtonsoft.Json;
-
-//namespace Ratings
-//{
-//    public static class CreateRating
-//    {
-//        [FunctionName("CreateRating")]
-//        public static void Run(
-//            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-//            [CosmosDB(
-//                databaseName: "openhack20",
-//                collectionName: "rating",
-//                PartitionKey = "/productID",
-//                ConnectionStringSetting = "CosmosDBConnection")]out dynamic document,
-//            ILogger log)
-//        {
-//            log.LogInformation("C# HTTP trigger function processed a request.");
-
-//            string productID = req.Query["productID"];
-
-//            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-//            //dynamic data = JsonConvert.DeserializeObject(requestBody);
-//            //name = name ?? data?.name;
-
-//            //return new OkResult();
-
-//            document = new { productID, id = Guid.NewGuid() };
-
-//        }
-//    }
-//}
-
 using System;
 using System.IO;
 using System.Threading.Tasks;
@@ -68,10 +28,6 @@ namespace Ratings
 
             document = null;
 
-            bool userIdChkFlg = false;
-            bool productIdChkFlg = false;
-            bool DbInsertErrorFlg = false;
-
             string userId = req.Query["userId"];
             string productId = req.Query["productId"];
             string locationName = req.Query["locationName"];
@@ -86,27 +42,24 @@ namespace Ratings
             ratings.userId = userId ?? data?.userId;
             ratings.productId = productId ?? data?.productId;
             ratings.locationName = locationName ?? data?.locationName;
-            ratings.rating = rating ?? data?.rating;
             ratings.userNotes = userNotes ?? data?.userNotes;
+            var ratingString = rating ?? data?.rating;
 
             if (!GetProduct(ratings.productId).Result.IsSuccessStatusCode)
-                return new BadRequestObjectResult("Product is not exist.");
+                return new NotFoundObjectResult("Product is not exist.");
 
             if (!GetUser(ratings.userId).Result.IsSuccessStatusCode)
-                return new BadRequestObjectResult("User is not exist.");
+                return new NotFoundObjectResult("User is not exist.");
 
-            
+            int rating_int = 0;
+            if (!int.TryParse(ratingString.ToString(), out rating_int))
+                return new BadRequestObjectResult("Rating is not integer.");
 
-            //userId notFound check
+            if (rating_int<0 || rating_int>5)
+                return new BadRequestObjectResult("Rating is out of range.");
 
-            //if(userIdChkFlg) return new NotFoundObjectResult("Please ...");
 
-            //productId notFound check
-
-            //if(productIdChkFlg) return new NotFoundObjectResult("Please ...");
-
-            //rating Check? êÆêîÇ©Ç«Ç§Ç©ÅAÇ‡ÇµÇ≠ÇÕêÆêîâª(ëSäpêîéöìô)
-
+            ratings.rating = rating_int;
 
             //create id
             ratings.id = Guid.NewGuid().ToString();
@@ -114,14 +67,7 @@ namespace Ratings
             //create timestamp
             ratings.timestamp = DateTime.UtcNow;
 
-            //DBInsert
-
-            //Inserté∏îséû
-            //if (DbInsertErrorFlg) return new UnprocessableEntityObjectResult("Please ....");
-
             document = ratings;
-
-            //TODO:return check
             return new OkObjectResult(document);
         }
 
@@ -146,6 +92,7 @@ namespace Ratings
             var azureFunctionUrl = $"https://serverlessohlondonuser.azurewebsites.net/api/GetUser?userId={userID}";
             return await new HttpClient().GetAsync(azureFunctionUrl);
         }
+
     }
 
     public class Ratings
