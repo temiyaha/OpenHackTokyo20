2b8fa75237f24d798474f8d48bedb0fdce189f21
Tetsuo Miyahara <temiyaha@microsoft.com>
2018-10-24 15:24:33 +0900

    update getrating (RatingItem class)

--- Ratings/Ratings/GetRating.cs
+++ Ratings/Ratings/GetRating.cs
@@ -45,9 +45,9 @@ namespace Ratings
         public string id { get; set; }
         public string userId { get; set; }
         public string productId { get; set; }
-        public string timestamp { get; set; }
+        public DateTime timestamp { get; set; }
         public string locationName { get; set; }
-        public string rating { get; set; }
+        public int rating { get; set; }
         public string userNotes { get; set; }
 
     }
